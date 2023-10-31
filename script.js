8134b7ea5e731d7aa34c69fcbc137a82e64ecf69
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-10-31 19:57:35 +0200

    created function to store user inputs inside an object containing arrays

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ const displayController = (() => {
     let playerOneOption = "";
     let playerTwoOption = "";
     let playerOneTurn = "";
+    let playerChoices = {
+        row1: [],
+        row2: [],
+        row3: [],
+    };
     playBtn.addEventListener("click", () => {
         let playersNodeList = document.querySelectorAll(".players");
         players = players(playersNodeList);
@@ -40,9 +45,13 @@ const displayController = (() => {
             } 
             else if (playerOneTurn === true) {
                 div.innerHTML = playerOneOption;
+                console.log(playerOneOption + div.id);
+                checkBoard(playerChoices, div.id, playerOneOption);
                 playerOneTurn = false;
             } else {
                 div.innerHTML = playerTwoOption;
+                console.log(playerTwoOption + div.id);
+                checkBoard(playerChoices, div.id, playerTwoOption);
                 playerOneTurn = true;
             };            
         });
@@ -54,4 +63,19 @@ function players (playersArray) {
     let player2 = playersArray[1].value;
     let playersOptions = ["X", "O"];
     return { player1, player2, playersOptions };
+};
+
+function checkBoard (playerChoices, boxId, userOption) {
+    // playerChoices["row" + boxId].splice(boxId - 1, 0, userOption);
+    if (boxId === "1" || boxId === "2" || boxId === "3") {
+        playerChoices.row1.splice(boxId - 1, 0, userOption);
+        console.log("Row 1 = " + playerChoices.row1.join(""));
+    }
+    else if (boxId === "4" || boxId === "5" || boxId === "6") {
+        playerChoices.row2.splice(boxId - 4, 0, userOption);
+        console.log("Row 2 = " + playerChoices.row2.join(""));
+    } else {
+        playerChoices.row3.splice(boxId - 7, 0, userOption);
+        console.log("Row 3 = " + playerChoices.row3.join(""));
+    };
 };
